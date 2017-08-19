008a6ec0217dfa901c3b1f9f1d033e64b5d65d22
mpppk <niboshiporipori@gmail.com>
2017-08-19 17:31:09 +0900

    Add stoppable http server

--- sosos/sosos.go
+++ sosos/sosos.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
 	"os/exec"
 	"time"
+
+	"github.com/hydrogen18/stoppableListener"
 )
 
 func logIfErrExist(err error) {
@@ -18,7 +22,7 @@ func Execute(commands []string, sleepSec int) {
 	cmd := exec.Command(commands[0], commands[1:]...)
 	stdout, err := cmd.StdoutPipe()
 	logIfErrExist(err)
-	time.Sleep(time.Duration(sleepSec) * time.Second)
+	waitWithCancelServer(sleepSec)
 	logIfErrExist(cmd.Start())
 	var person struct {
 		Name string
@@ -27,4 +31,32 @@ func Execute(commands []string, sleepSec int) {
 	logIfErrExist(json.NewDecoder(stdout).Decode(&person))
 	logIfErrExist(cmd.Wait())
 	fmt.Printf("%s is %d years old\n", person.Name, person.Age)
+}
+
+func Hello(rw http.ResponseWriter, req *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprintf(rw, "Hello\n")
+}
+
+func waitWithCancelServer(sleepSec int) {
+	l, err := net.Listen("tcp", ":3333")
+	if err != nil {
+		panic(err)
+	}
+	sl, err := stoppableListener.New(l)
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", Hello)
+	s := http.Server{}
+
+	go func() {
+		s.Serve(sl)
+	}()
+	fmt.Printf("Serving HTTP\n")
+
+	time.Sleep(time.Duration(sleepSec) * time.Second)
+
+	fmt.Println("stopping server..")
+	sl.Stop()
 }
