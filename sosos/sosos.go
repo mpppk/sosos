70b8342a6995b2d8d753f2274ced9a3761954013
mpppk <niboshiporipori@gmail.com>
2017-08-20 14:52:16 +0900

    Post command execution start message and result to slack

--- sosos/sosos.go
+++ sosos/sosos.go
@@ -54,6 +54,19 @@ func getCancelServerUrl(insecureFlag bool, port int) (string, error) {
 	return fmt.Sprintf("%s://%s:%d", protocol, hostname, port), nil
 }
 
+func postMessageToSlack(url, message string) (*http.Response, error) {
+	content, err := json.Marshal(SlackWebhookContent{Text: message})
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.Post(url, "application/json", bytes.NewReader(content))
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 func Execute(commands []string, sleepSec int, port int, insecureFlag bool, webhookUrl string) error {
 	cancelServerUrl, err := getCancelServerUrl(insecureFlag, port)
 	if err != nil {
@@ -74,15 +87,12 @@ func Execute(commands []string, sleepSec int, port int, insecureFlag bool, webho
 	message += fmt.Sprintf("[Cancel](%s)", cancelServerUrl)
 
 	// webhook
-	content, err := json.Marshal(SlackWebhookContent{Text: message})
+	res, err := postMessageToSlack(webhookUrl, message)
 	if err != nil {
 		return err
 	}
 
-	res, err := http.Post(webhookUrl, "application/json", bytes.NewReader(content))
-	if err != nil {
-		return err
-	}
+	fmt.Println("http response " + res.Status)
 
 	isCanceled, err := waitWithCancelServer(sleepSec, port, insecureFlag, webhookUrl)
 	if err != nil {
@@ -92,6 +102,13 @@ func Execute(commands []string, sleepSec int, port int, insecureFlag bool, webho
 	if !isCanceled {
 		message := "Command execution is started!"
 		fmt.Println(message)
+		res, err := postMessageToSlack(webhookUrl, message)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("http response " + res.Status)
+
 		out, err := exec.Command(commands[0], commands[1:]...).CombinedOutput()
 		if err != nil {
 			return err
@@ -100,10 +117,16 @@ func Execute(commands []string, sleepSec int, port int, insecureFlag bool, webho
 		fmt.Println("---- command output ----")
 		fmt.Println(string(out))
 		fmt.Println("---- command output ----")
-		// 結果をwebhookで通知
+
+		resultMessage := fmt.Sprintf("result:\n```%s```", string(out))
+		resultRes, err := postMessageToSlack(webhookUrl, resultMessage)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("http response " + resultRes.Status)
 	} else {
 		fmt.Println("command is canceled")
-		// cancelされたことをwebhookで通知
 
 	}
 	return nil
